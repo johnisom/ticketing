d76da685a7f5a78e8a580075e1fc74d637693430
John Isom <John.Isom2001@gmail.com>
2020-05-05 15:47:22 -0600

    Add scaffold for tags

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'projects#index'
+
   resources :projects
   resources :tickets
+  resources :tags, except: :show
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_05_193116) do
+ActiveRecord::Schema.define(version: 2020_05_05_213451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 2020_05_05_193116) do
   create_table "projects", force: :cascade do |t|
     t.string "name", null: false
     t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
